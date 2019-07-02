9e3f40a54bc2564d4d740993fdaeac53e6ce1dac
Susie Kim <susiekim@Susies-MacBook-Pro.local>
2019-07-01 21:55:09 -0400

    Adding deployment configuration, replacing router switch with homepage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Homepage from "./components/Homepage.js";
-import Login from "./components/Login.js";
-import Register from "./components/Register.js";
-import "./App.scss";
+import "./components/Homepage.css";
+import {Card, CardBody} from "reactstrap";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route
-					exact
-					path="/"
-					name="Homepage"
-					render={props => <Homepage {...props} />}
-				/>
-				<Route
-					exact
-					path="/login"
-					name="Login"
-					render={props => <Login {...props} />}
-				/>
-				<Route
-					exact
-					path="/register"
-					name="Register"
-					render={props => <Register {...props} />}
-				/>
-			</Switch>
-		</BrowserRouter>
+		<Card style={{ width: "25%" }}>
+			<CardBody>
+				<div style={{ fontSize: "30px" }}>Retro</div>
+			</CardBody>
+		</Card>
 	);
 }
 
