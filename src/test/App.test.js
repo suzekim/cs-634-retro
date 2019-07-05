6feb26b21b49baa79a16bc5bacd41bcc2a2b2a92
Susie Kim <susiekim21@gmail.com>
2019-07-05 19:35:20 -0400

    Adding testSetup and first test case

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../main/App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../main/App';
 
-it("renders without crashing", () => {
-	const div = document.createElement("div");
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+it('renders Retro header', () => {
+	const wrapper = shallow(<App />);
+	expect(wrapper.contains("Retro")).toEqual(true);
 });
