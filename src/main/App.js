48a0d3a36a9a9fe9d62f3c9ab8f6bfb68b11bc3a
Susie Kim <susiekim21@gmail.com>
2019-07-05 21:21:57 -0400

    Adding text box for inputting feedback.

--- src/main/App.js
+++ src/main/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./components/Homepage.css";
-import {Card, CardBody} from "reactstrap";
+import {Card, CardBody, InputGroup, Input} from "reactstrap";
 
 function App() {
 	return (
-		<Card style={{ width: "25%" }}>
-			<CardBody>
-				<div style={{ fontSize: "30px" }}>Retro</div>
-			</CardBody>
-		</Card>
+		<div>
+			<Card style={{width: "25%"}}>
+				<CardBody>
+					<div style={{fontSize: "30px"}}>Retro</div>
+				</CardBody>
+			</Card>
+			<InputGroup className="input">
+				<Input placeholder="What went well? What could have gone better?"/>
+			</InputGroup>
+		</div>
 	);
 }
 
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../main/App';
 
-it('renders Retro header', () => {
+
+it('Retro Header', () => {
 	const wrapper = shallow(<App />);
 	expect(wrapper.contains("Retro")).toEqual(true);
+});
+
+describe('Input', function () {
+	const wrapper = shallow(<App/>);
+	it('has input group for text', () => {
+		const inputGroup = wrapper.find('InputGroup').dive();
+
+		expect(inputGroup.hasClass('input-group')).toBe(true);
+	});
+	it('has placeholder text for feedback', () => {
+		const input = wrapper.find('Input');
+		const placeholder = input.props().placeholder;
+
+		expect(placeholder).toEqual('What went well? What could have gone better?');
+	});
 });
