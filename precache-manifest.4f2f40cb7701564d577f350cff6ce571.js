0e81b56752f594099f2390598b68d04ed559990e
Susie Kim <susiekim21@gmail.com>
2019-07-09 22:06:11 -0400

    Updates

---
+++ precache-manifest.4f2f40cb7701564d577f350cff6ce571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb830b02d5ec2925c5a4",
+    "url": "/cs-634-retro/static/css/main.0472e00c.chunk.css"
+  },
+  {
+    "revision": "bb830b02d5ec2925c5a4",
+    "url": "/cs-634-retro/static/js/main.41b0dab4.chunk.js"
+  },
+  {
+    "revision": "ca9527c57e406bc99f9a",
+    "url": "/cs-634-retro/static/js/runtime~main.9971873c.js"
+  },
+  {
+    "revision": "811c7f8b4ab83e3062d6",
+    "url": "/cs-634-retro/static/js/2.871929ed.chunk.js"
+  },
+  {
+    "revision": "c9e225d2266daa775a2450049f50682a",
+    "url": "/cs-634-retro/index.html"
+  }
+];
